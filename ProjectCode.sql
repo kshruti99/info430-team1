a376659f8c765b5ccc43e357f0df995723cdc430
smckissick1 <55968658+smckissick1@users.noreply.github.com>
2021-11-16 18:25:59 -0800

    Update ProjectCode.sql

--- ProjectCode.sql
+++ ProjectCode.sql
@@ -1,1 +1,19 @@
--- starter SQL file to write all code in created in hay09
+/*
+starter SQL file to write all code in created in hay09
+
+Each team member must code 2 of each of the following (8 objects per student):
+
+1) Stored procedure
+2) Check constraint
+3) Computed column
+4) Views
+
+As stated in lecture, grading will be based on the student's ability to leverage complex skills presented in lecture and should include the following where appropriate:
+
+* explicit transactions
+* Complexity as appropriate: multiple JOINs, GROUP BY, ORDER BY, TOP, RANK, CROSS APPLY
+* error-handling
+* passing of appropriate  parameters (name values and/or output parameters)
+* subqueries
+* variables
+*/
