5b5e06aa306d60e5043c7f6a8a48965ee03c0e13
smckissick1 <55968658+smckissick1@users.noreply.github.com>
2021-11-18 14:17:39 -0800

    Update SierrasCode.sql

--- SierrasCode.sql
+++ SierrasCode.sql
@@ -3,8 +3,6 @@ Use INFO_430_Proj_01;
 insert into tblSTOP_DIRECTION(DirectionName)
 values ('Northbound'), ('Southbound'), ('Westbound'), ('Eastbound')
 
-select * from tblPassenger_Type
-
 Create Procedure GetDirectionID
 @DirectionNamey varchar(50),
 @Directiony INT OUTPUT
