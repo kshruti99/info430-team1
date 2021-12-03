653539a50eebad327434285e036e3072c0f22752
Justin Chen <chenz249@uw.edu>
2021-12-02 22:50:53 -0800

    Added update vehicle stored procedure; Modified tblVEHICLE and related stored procedure

--- Proj4Justin.sql
+++ Proj4Justin.sql
@@ -2,10 +2,9 @@ Use INFO_430_Proj_01;
 
 -- Update vehicle Stored Procedure
 CREATE PROCEDURE UpdateVehicle @VID INT,
-                               @VTypename varchar(50),
-                               @RtName varchar(50)
+                               @VTypename varchar(50)
 AS
-DECLARE @VT_ID INT, @Rt_ID INT
+DECLARE @VT_ID INT
     EXEC GetVehicleTypeID
          @VTname = @VTypename,
          @VeTy = @VT_ID OUTPUT
@@ -17,21 +16,9 @@ DECLARE @VT_ID INT, @Rt_ID INT
             RETURN
         END
 
-    EXEC GetRouteID
-         @RName = @RtName,
-         @Routy = @Rt_ID OUTPUT
--- Error handling goes here
-    IF @Rt_ID IS NULL
-        BEGIN
-            PRINT 'Route name is null, please check!'
-            RAISERROR ('@Rt_ID is throwing an error, please check', 11, 1);
-            RETURN
-        END
-
     BEGIN TRAN T1
 UPDATE tblVEHICLE
-SET VehicleTypeID = @VT_ID,
-    RouteID       = @Rt_ID
+SET VehicleTypeID = @VT_ID
 WHERE VehicleID = @VID
     IF @@ERROR <> 0
         BEGIN
@@ -50,5 +37,5 @@ GO
 -- SELECT * FROM tblVEHICLE WHERE VehicleID = 2;
 
 -- Business Rules
--- Rule1:
--- Rule2:
+-- Rule1: Employees who are younger than 35 years old cannot be on the Route Name with 'B' and 'Q'
+-- Rule2: No more than Vehicle Count > 5 in any neighborhood (VEHICLE)
