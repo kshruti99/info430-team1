30d32da15012dbcc628d860ce4122046091c63d0
smckissick1 <55968658+smckissick1@users.noreply.github.com>
2021-12-06 14:33:32 -0800

    whoops accidental update to wrong file

--- Proj4Justin.sql
+++ Proj4Justin.sql
@@ -1,340 +1,169 @@
 Use INFO_430_Proj_01;
--- getIDs needed for sproc 1
-Create Procedure GetDirectionID
-@DirectionNamey varchar(50),
-@Directiony INT OUTPUT
+
+-- Update vehicle Stored Procedure
+CREATE PROCEDURE UpdateVehicle @VID INT,
+                               @VTypename varchar(50)
 AS
-SET @Directiony = (SELECT DirectionID FROM tblSTOP_DIRECTION WHERE DirectionName = @DirectionNamey)
+DECLARE @VT_ID INT
+    EXEC GetVehicleTypeID
+         @VTname = @VTypename,
+         @VeTy = @VT_ID OUTPUT
+-- Error handling goes here
+    IF @VT_ID IS NULL
+        BEGIN
+            PRINT 'Vehicle type name is null, please check!'
+            RAISERROR ('@VT_ID is throwing an error, please check', 11, 1);
+            RETURN
+        END
+
+    BEGIN TRAN T1
+UPDATE tblVEHICLE
+SET VehicleTypeID = @VT_ID
+WHERE VehicleID = @VID
+    IF @@ERROR <> 0
+        BEGIN
+            ROLLBACK TRAN T1
+        END
+    ELSE
+        COMMIT TRAN T1
 GO
 
-ALTER Procedure GetNeighborhoodID
-@NeighborhoodNamey varchar(50),
-@Zipy varchar(10),
-@Neighborhoody INT OUTPUT
-AS
-SET @Neighborhoody = (SELECT NeighborhoodID FROM tblNEIGHBORHOOD WHERE NeighborhoodName = @NeighborhoodNamey AND ZipCode = @Zipy)
-GO
 
---sproc1
-CREATE PROCEDURE INSERT_STOP
-@N_Name varchar(50),
-@D_Name varchar(50),
-@StopName varchar(50),
-@ZIP varchar(10)
-AS
-DECLARE
-@N_ID INT, @D_ID INT
+-- ALTER TABLE tblVEHICLE
+-- ADD VehicleName varchar(50) NOT NULL DEFAULT 'tbd'
 
-EXEC GetNeighborhoodID
-@NeighborhoodNamey = @N_Name,
-@Zipy = @ZIP,
-@Neighborhoody = @N_ID OUTPUT
 
-IF @N_ID IS NULL
-BEGIN
-PRINT 'Neighborhood ID IS NULL'
-RAISERROR ('CHECK NEIGHBORHOOD ID', 11, 1)
-RETURN
-END
+-- Test
+-- EXEC UpdateVehicle
+-- @VID = 2,
+--     @VTypename = 'Single-decker bus',
+--     @RtName = 'M96';
+-- SELECT * FROM tblVEHICLE WHERE VehicleID = 2;
 
-EXEC GetDirectionID
-@DirectionNamey = @D_Name,
-@Directiony = @D_ID OUTPUT
-
-IF @D_ID IS NULL
-BEGIN
-	PRINT 'Direction ID IS NULL'
-	RAISERROR ('CHECK SPELLING OF Direction AS THERE IS AN ERROR', 11, 1)
-	RETURN
-END
-
-BEGIN TRAN T1
-INSERT INTO tblSTOP (NeighborhoodID, DirectionID, StopName)
-VALUES (@N_ID, @D_ID, @StopName)
-IF @@ERROR <> 0
- BEGIN
-     ROLLBACK TRAN T1
- END
-ELSE
- COMMIT TRAN T1
-GO
-
---GetIDs needed for sproc 2
-CREATE PROCEDURE GetRouteID
-@RName varchar(50),
-@Routy INT OUTPUT
-AS
-SET @Routy = (SELECT RouteID
-              FROM tblROUTE
-              WHERE RouteName = @RName)
-GO
-
-ALTER PROCEDURE GetVehicleID
-@VName varchar(30),
-@VID INT OUTPUT
-AS
-SET @VID = (SELECT vehicleID FROM tblVEHICLE
-        WHERE VehicleName = @VName)
-GO
-SELECT DISTINCT vehicleName FROM tblVEHICLE
-ALTER PROCEDURE GetEmployeeID
-@EFirstName varchar(30),
-@ELastName varchar(30),
-@EDOB Date,
-@Empy INT OUTPUT
-AS
-SET @Empy = (SELECT EmployeeID FROM tblEMPLOYEE WHERE EmployeeFirstName = @EFirstName AND
-    EmployeeLastName = @ELastName AND EmployeeDOB = @EDOB)
-GO
-
-ALTER PROCEDURE GetTransportationID
-@TranName varchar(30),
-@TID INT OUTPUT
-AS
-SET @TID = (SELECT TransportationID FROM tblTRANSPORTATION WHERE transportationName = @TranName)
-GO
-
-ALTER PROCEDURE GetPassengerID
-@Pemail varchar(50),
-@Passengery INT OUTPUT
-AS
-SET @Passengery = (SELECT passengerID FROM tblPASSENGER WHERE PassengerEmail = @Pemail)
-GO
-
-ALTER PROCEDURE GetNeighborhoodID
-@NName varchar(50),
-@ZCode INT,
-@Neighy INT OUTPUT
-AS
-SET @Neighy = (SELECT neighborhoodID FROM tblNEIGHBORHOOD WHERE NeighborhoodName = @NName
-                AND ZipCode = @ZCode)
-GO
-
-ALTER PROCEDURE GetStopID
-@SName varchar(30),
-@SID INT OUTPUT
-AS
-SET @SID = (SELECT stopID FROM tblSTOP S
-            WHERE S.stopName = @SName)
-GO
-
---sproc 2
-ALTER PROCEDURE INSERT_BOARDING
-@TranspName varchar(30),
-@Pmail2 varchar(50),
-@SN2 varchar(30)
-AS
-DECLARE
-@T_ID INT, @P_ID INT, @S_ID INT
-
-EXEC GetPassengerID
-@Pemail = @Pmail2,
-@Passengery = @P_ID OUTPUT
-IF @P_ID IS NULL
-BEGIN
-PRINT 'passenger ID IS NULL'
-RAISERROR ('CHECK passenger ID', 11, 1)
-RETURN
-END
-
-EXEC GetStopID
-@SName = @SN2,
-@SID = @S_ID OUTPUT
-IF @S_ID IS NULL
-BEGIN
-PRINT 'stop ID IS NULL'
-RAISERROR ('CHECK stop ID', 11, 1)
-RETURN
-END
-
-EXEC GetTransportationID
-@TranName = @TranspName,
-@TID =  @T_ID OUTPUT
-IF @T_ID IS NULL
-BEGIN
-PRINT 'transportation ID IS NULL'
-RAISERROR ('CHECK transportation ID', 11, 1)
-RETURN
-END
-
-BEGIN TRAN T1
-INSERT INTO tblBOARDING (TransportationID, PassengerID, StopID)
-VALUES (@T_ID, @P_ID, @S_ID)
-IF @@ERROR <> 0
- BEGIN
-     ROLLBACK TRAN T1
- END
-ELSE
- COMMIT TRAN T1
-GO
-
---synthetic transaction to populate tblBOARDING
-ALTER PROCEDURE Wrapper_INSERT_BOARDING
-@RUN INT
-AS
-DECLARE @P_PK INT
-DECLARE @S_PK INT
-DECLARE @T_PK INT
-DECLARE @P_COUNT INT = (SELECT COUNT(*) FROM tblPASSENGER)
-DECLARE @S_COUNT INT = (SELECT COUNT(*) FROM tblSTOP)
-DECLARE @T_COUNT INT = (SELECT COUNT(*) FROM tblTRANSPORTATION)
-DECLARE @T_name varchar(30), @Pmail3 varchar(50), @SN3 varchar(30)
-
-WHILE @RUN > 0
-BEGIN
-
-    SET @P_PK = (SELECT RAND() * @P_COUNT + 1)
-    SET @Pmail3 = (SELECT PassengerEmail FROM tblPASSENGER WHERE PassengerID = @P_PK)
-
-    SET @S_PK = (SELECT RAND() * @S_COUNT + 1 + 13624)
-    SET @SN3 = (SELECT StopName FROM tblSTOP WHERE stopID = @S_PK)
-
-    SET @T_PK = (SELECT RAND() * @T_COUNT + 1 + 8384)
-    SET @T_name = (SELECT transportationName FROM tblTRANSPORTATION WHERE TransportationID = @T_PK)
-
-EXEC INSERT_BOARDING
-@TranspName = @T_name,
-@Pmail2 = @Pmail3,
-@SN2 = @SN3
-
-SET @RUN = @RUN -1
-END
-GO
-
-EXEC Wrapper_INSERT_BOARDING
-    @RUN = 3000
-SELECT * FROM tblBOARDING
-SELECT * FROM tblTRANSPORTATION
-SELECT COUNT(vehicleName) FROM tblVEHICLE
-SELECT COUNT(DISTINCT vehicleName) FROM tblVEHICLE
-EXEC wrapper_insertXport 10
-
---business rule 1 passengers cannot be under 10 and ride the bus in certain neighborhoods going south
-CREATE FUNCTION dbo.proj01_underAge()
+-- Business Rules
+-- Rule1: Employees who are younger than 35 years old cannot be on the Route Name with 'B' and 'Q'
+CREATE FUNCTION dbo.proj01_underageEmployeeforBandQRoute()
 RETURNS INTEGER
 AS
-BEGIN
-DECLARE @RET INTEGER = 0
-    IF EXISTS(SELECT *
-              FROM tblPASSENGER P
-              JOIN tblBOARDING B ON P.PassengerID = B.PassengerID
-              JOIN tblSTOP S ON S.StopID = B.StopID
-              JOIN tblNEIGHBORHOOD N ON N.NeighborhoodID = S.NeighborhoodID
-              JOIN tblSTOP_DIRECTION SD on S.DirectionID = SD.DirectionID
-              WHERE P.PassengerDOB <= DATEADD(YEAR, -10, GETDATE())
-                AND SD.DirectionName <> 'Southbound'
-                AND N.NeighborhoodName <> 'Central Bronx'
-                AND N.NeighborhoodName <> 'High Bridge and Morrisania')
-              BEGIN
-                SET @RET = 1
-              end
-    RETURN @RET
-END
-GO
-
-ALTER TABLE tblPASSENGER with nocheck
-ADD CONSTRAINT const_underAge
-CHECK (dbo.proj01_underAge() = 0)
-
--- business rule 2 School buses cannot have any passengers over 22 and must have edu emails
-CREATE FUNCTION dbo.proj01_maxPassengers()
-RETURNS INTEGER
-AS
-BEGIN
-    DECLARE @RET INTEGER = 0
-    IF EXISTS(SELECT * FROM tblBOARDING B
-        JOIN tblPASSENGER P ON P.PassengerID = B.PassengerID
-        JOIN tblTRANSPORTATION T on B.TransportationID = T.TransportationID
-        JOIN tblVEHICLE V ON V.VehicleID = T.VehicleID
-        JOIN tblVEHICLE_TYPE VT on V.VehicleTypeID = VT.VehicleTypeID
-        WHERE P.PassengerDOB > DATEADD(YEAR, -22, GETDATE())
-        AND VT.VehicleTypeName = 'School bus'
-        AND P.PassengerEmail LIKE '%.edu')
+    BEGIN
+        DECLARE @RET INT = 0
+        IF EXISTS(SELECT * FROM tblEMPLOYEE E
+            JOIN tblTRANSPORTATION TR ON E.EmployeeID = TR.EmployeeID
+            JOIN tblROUTE R ON TR.RouteID = R.RouteID
+            WHERE R.RouteName LIKE 'B%' OR R.RouteName LIKE 'O%'
+            AND E.EmployeeDOB < DATEADD(YEAR, -35, GETDATE()))
         BEGIN
             SET @RET = 1
         END
-    RETURN @RET
-end
-GO
-
-ALTER TABLE tblBOARDING with nocheck
-ADD CONSTRAINT const_maxBoarding
-CHECK (dbo.proj01_maxPassengers() = 0)
-
--- computed column 1 Stop use frequency
-CREATE FUNCTION fn_stopPop(@PK INT)
-RETURNS varchar(30)
-    AS
-    BEGIN
-    DECLARE @RET varchar(30) = (SELECT COUNT(B.BoardingID) as stopCount FROM tblBOARDING B
-                                JOIN tblSTOP S ON S.StopID = B.StopID
-                                WHERE S.StopID = @PK)
         RETURN @RET
-    end
-    GO
+    END
+GO
 
-	ALTER TABLE tblSTOP
-	ADD fn_stopPop
-	AS (DBO.fn_stopPop(StopID))
+ALTER TABLE tblTRANSPORTATION WITH NOCHECK
+ADD CONSTRAINT underageEmployeeforBandQRoute
+CHECK (dbo.proj01_underageEmployeeforBandQRoute() = 0)
 
--- computed column 2 number of stops in each direction
-CREATE FUNCTION fn_DirectionPop(@PK varchar(30))
-RETURNS varchar(30)
-    AS
+-- Rule2: No more than 5 Vehicles with type Double-decker bus in any neighborhood (VEHICLE)
+CREATE FUNCTION dbo.proj01_nomore5VehiclesinAnyNeighborhood()
+RETURNS INTEGER
+AS
     BEGIN
-    DECLARE @RET varchar(30) = (SELECT COUNT(S.stopID) FROM tblSTOP S
-                                JOIN tblSTOP_DIRECTION SD on S.DirectionID = SD.DirectionID
-                                WHERE SD.DirectionName = @PK)
+        DECLARE @RET INT = 0
+        IF EXISTS(SELECT COUNT(*) FROM tblVEHICLE V
+            JOIN tblTRANSPORTATION TR ON V.VehicleID = TR.VehicleID
+            JOIN tblVEHICLE_TYPE VT ON V.VehicleTypeID = VT.VehicleTypeID
+            WHERE VT.VehicleTypeName = 'Double-decker bus'
+            GROUP BY V.VehicleID
+            HAVING COUNT(*) > 5)
+        BEGIN
+            SET @RET = 1
+        END
         RETURN @RET
-    end
-    GO
-
-	ALTER TABLE tblSTOP_DIRECTION
-	ADD fn_TopNeighborhood
-	AS (DBO.fn_DirectionPop(DirectionName))
-
--- View1 - ages and names of the youngest five percent of water taxi inspectors
-CREATE VIEW vwYoungest_WaterTaxiInspectors
-AS
-SELECT A.EmployeeFirstName, A.EmployeeLastName, A.EmployeeAge FROM
-       (SELECT NTILE(100) OVER (ORDER BY E.EmployeeDOB DESC) AS waterTaxiInspector,
-        E.EmployeeFirstName, E.EmployeeLastName, E.EmployeeDOB, DATEDIFF(YEAR, E.EmployeeDOB, GETDATE()) AS EmployeeAge
-FROM tblEMPLOYEE E
-JOIN tblEMPLOYEE_TYPE ET on E.EmployeeTypeID = ET.EmployeeTypeID
-WHERE ET.EmployeeTypeName = 'water taxi inspector') AS A
-WHERE A.waterTaxiInspector <= 5
+    END
 GO
 
--- View2 - top 3 oldest employees of each vehicle type
-CREATE VIEW vwOldest_Employees_VType
+ALTER TABLE tblTRANSPORTATION WITH NOCHECK
+ADD CONSTRAINT nomore5VehiclesinAnyNeighborhood
+CHECK (dbo.proj01_nomore5VehiclesinAnyNeighborhood() = 0)
+
+-- Computed Column
+-- Column1: The number of vehicles of each vehicle type
+CREATE FUNCTION fn_VehicleCount(@PK INT)
+RETURNS INTEGER
 AS
-SELECT A.employeeFirstName, A.employeeLastName, A.EmployeeTypeName, A.EmployeeDOB FROM
-                   (SELECT RANK() OVER (PARTITION BY ET.employeeTypeName ORDER BY E.EmployeeDOB)
-                    AS oldEmps, E.employeeFirstName, E.employeeLastName, ET.EmployeeTypeName, E.EmployeeDOB
-FROM tblEMPLOYEE E
-    JOIN tblEMPLOYEE_TYPE ET on E.EmployeeTypeID = ET.EmployeeTypeID) as A
-	WHERE oldEmps <= 3
+    BEGIN
+        DECLARE @RET INT = (SELECT COUNT(*) FROM tblVEHICLE V
+            JOIN tblVEHICLE_TYPE VT ON V.VehicleTypeID = VT.VehicleTypeID
+            WHERE V.VehicleTypeID = @PK)
+        RETURN @RET
+    END
 GO
 
--- for presentation:
--- visualization 1
-SELECT A.EmployeeFirstName, A.EmployeeLastName, A.EmployeeAge, A.EmployeeTypeName FROM
-        (SELECT E.EmployeeFirstName, E.EmployeeLastName,
-E.EmployeeDOB, DATEDIFF(YEAR, E.EmployeeDOB, GETDATE()) AS EmployeeAge, ET.EmployeeTypeName
-FROM tblEMPLOYEE E
-JOIN tblEMPLOYEE_TYPE ET on E.EmployeeTypeID = ET.EmployeeTypeID
-WHERE ET.EmployeeTypeName = 'water taxi inspector') AS A
+ALTER TABLE tblVEHICLE_TYPE
+ADD VehicleCount
+AS (dbo.fn_VehicleCount(VehicleTypeID))
 
--- visualization 2
-SELECT * FROM vwPassengerBoardingCount
+-- Column2: Average age of passengers in each transportation
+CREATE FUNCTION fn_AveragePassengerAgeEachTransportation(@PK INT)
+RETURNS INT
+AS
+    BEGIN
+        DECLARE @RET INT = (SELECT AVG(DATEDIFF(YEAR, P.PassengerDOB, GETDATE()))
+            FROM tblPASSENGER P
+            JOIN tblBOARDING B ON P.PassengerID = B.PassengerID
+            JOIN tblTRANSPORTATION TR ON B.TransportationID = TR.TransportationID
+            WHERE TR.TransportationID = @PK
+            GROUP BY P.PassengerID)
+        RETURN @RET
+    END
+GO
 
---visualization 3
-SELECT VT.VehicleTypeID, VT. VehicleTypeName, COUNT(B.boardingID) AS riderCount
+ALTER TABLE tblTRANSPORTATION
+ADD AveragePassengerAge
+AS (dbo.fn_AveragePassengerAgeEachTransportation(TransportationID))
+
+-- Views
+-- View1:
+CREATE VIEW vwPassengerBoardingCount
+AS
+    SELECT (CASE
+        WHEN HybridBoardingCount > 500
+        THEN 'Environment Friendly'
+        WHEN HybridBoardingCount BETWEEN 200 AND 500
+        THEN 'Becoming Environment Friendly'
+        WHEN HybridBoardingCount BETWEEN 100 AND 200
+        THEN 'Changing Mind'
+        ELSE 'Traditional Riders'
+        END) AS LabelsForPassengers, COUNT(*) AS NumberOfPassengers
+FROM (
+SELECT P.PassengerID, COUNT(B.BoardingID) AS HybridBoardingCount
+FROM tblPASSENGER P
+JOIN tblBOARDING B ON P.PassengerID = B.PassengerID
+JOIN tblTRANSPORTATION TR ON B.TransportationID = TR.TransportationID
+JOIN tblVEHICLE V ON TR.VehicleID = V.VehicleID
+JOIN tblVEHICLE_TYPE VT ON V.VehicleTypeID = VT.VehicleTypeID
+WHERE VT.VehicleTypeName = 'Hybrid bus'
+GROUP BY P.PassengerID) AS A
+
+GROUP BY (CASE
+        WHEN HybridBoardingCount > 500
+        THEN 'Environment Friendly'
+        WHEN HybridBoardingCount BETWEEN 200 AND 500
+        THEN 'Becoming Environment Friendly'
+        WHEN HybridBoardingCount BETWEEN 100 AND 200
+        THEN 'Changing Mind'
+        ELSE 'Traditional Riders'
+        END)
+GO
+
+-- View2:
+CREATE VIEW vwMostPopularVehicleTypes
+AS
+    SELECT VT.VehicleTypeID, VT.VehicleTypeName,
+           DENSE_RANK() OVER (ORDER BY (COUNT(B.BoardingID))) AS D_Rank
 FROM tblVEHICLE_TYPE VT
 JOIN tblVEHICLE V ON VT.VehicleTypeID = V.VehicleTypeID
 JOIN tblTRANSPORTATION TR ON V.VehicleID = TR.VehicleID
 JOIN tblBOARDING B ON TR.TransportationID = B.TransportationID
 GROUP BY VT.VehicleTypeID, VT.VehicleTypeName
 GO
-
-
