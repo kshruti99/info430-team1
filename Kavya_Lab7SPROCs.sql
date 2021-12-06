9ae0754702b5105f0269d9067866d9f2212463a3
smckissick1 <55968658+smckissick1@users.noreply.github.com>
2021-12-06 13:09:52 -0800

    Update Kavya_Lab7SPROCs.sql

--- Kavya_Lab7SPROCs.sql
+++ Kavya_Lab7SPROCs.sql
@@ -10,15 +10,15 @@ IF EXISTS (SELECT * FROM sys.sysobjects WHERE Name = 'WORKING_PEEPS_Customers')
 	BEGIN
 		DROP TABLE WORKING_PEEPS_Customers
 
-		SELECT TOP 7000 * 
+		SELECT TOP 7000 *
 		INTO WORKING_PEEPS_Customers
 		FROM PEEPS.dbo.tblCUSTOMER
 	END
 
 
 
-CREATE PROCEDURE proj01_getPassengerTypeID 
-@PTName varchar(50), 
+CREATE PROCEDURE proj01_getPassengerTypeID
+@PTName varchar(50),
 @Passenger_Typey INT OUTPUT
 AS
 SET @Passenger_Typey = (Select PassengerTypeID from tblPassenger_Type WHERE PassengerTypeName = @PTName)
@@ -26,49 +26,49 @@ SET @Passenger_Typey = (Select PassengerTypeID from tblPassenger_Type WHERE Pass
 
 -- base stored proc
 CREATE PROCEDURE insertPassenger
-@Firsty varchar(50), 
-@Lasty varchar(50), 
-@Birthy DATE, 
-@Emaily varchar(50), 
+@Firsty varchar(50),
+@Lasty varchar(50),
+@Birthy DATE,
+@Emaily varchar(50),
 @ptypeName varchar(50)
-AS 
+AS
 DECLARE @TYPY_ID INT
 
-EXEC  proj01_getPassengerTypeID 
-@PTName = @ptypeName, 
-@Passenger_Typey = @TYPY_ID OUTPUT 
+EXEC  proj01_getPassengerTypeID
+@PTName = @ptypeName,
+@Passenger_Typey = @TYPY_ID OUTPUT
 
 -- error handling
-IF @TYPY_ID IS NULL 
-	BEGIN 
+IF @TYPY_ID IS NULL
+	BEGIN
 		PRINT 'PASSENGER TYPE ID IS NULL, PLEASE FIX'
 		RAISERROR ('CHECK SPELLING OF PSNGR TYPE', 11, 1)
 		RETURN
-	END 
+	END
 
 BEGIN TRAN T1
-INSERT INTO TBLPASSENGER(PassengerTypeID, PassengerFirstName, PassengerLastName, PassengerDOB, 
+INSERT INTO TBLPASSENGER(PassengerTypeID, PassengerFirstName, PassengerLastName, PassengerDOB,
 						PassengerEmail)
 	VALUES (@TYPY_ID, @FIRSTY, @LASTY, @BIRTHY, @EMAILY)
 
-IF @@ERROR <> 0 
-	BEGIN 
-		ROLLBACK TRAN T1 
+IF @@ERROR <> 0
+	BEGIN
+		ROLLBACK TRAN T1
 	END
-ELSE 
+ELSE
 	COMMIT TRAN T1
-GO 
+GO
 
 exec insertPassenger
-@Firsty = 'Kavya', 
-@Lasty = 'Iyer', 
-@Birthy = '1999-02-11', 
-@Emaily = 'kavyai@uw.edu', 
+@Firsty = 'Kavya',
+@Lasty = 'Iyer',
+@Birthy = '1999-02-11',
+@Emaily = 'kavyai@uw.edu',
 @ptypeName = 'Student'
 select * from tblPASSENGER
 
 
--- write wrapper 
+-- write wrapper
 ALTER PROCEDURE wrapper_insertPassenger
 @RUN INT
 AS
@@ -76,13 +76,13 @@ DECLARE @PT_PK INT
 DECLARE @PT_COUNT INT = (SELECT COUNT(*) FROM TBLPASSENGER_TYPE)
 DECLARE @PEEP_PK INT
 DECLARE @PEEP_COUNT INT = (SELECT COUNT(*) FROM WORKING_PEEPS_Customers)
-DECLARE @FNAME varchar(50), @LNAME varchar(50), @DOBBY DATE, @PEMAIL varchar(50), 
+DECLARE @FNAME varchar(50), @LNAME varchar(50), @DOBBY DATE, @PEMAIL varchar(50),
 @PASSTypName varchar(50)
 
-WHILE @RUN > 0 
-BEGIN 
-SET @PT_PK = (SELECT RAND() * @PT_COUNT + 1) 
-SET @PEEP_PK = (SELECT MIN(CustomerID) FROM WORKING_PEEPS_Customers) 
+WHILE @RUN > 0
+BEGIN
+SET @PT_PK = (SELECT RAND() * @PT_COUNT + 1)
+SET @PEEP_PK = (SELECT MIN(CustomerID) FROM WORKING_PEEPS_Customers)
 SET @PASSTypName = (SELECT PassengerTypeName from tblPASSENGER_TYPE WHERE PassengerTypeID = @PT_PK)
 SET @FNAME = (SELECT CustomerFName FROM WORKING_PEEPS_Customers WHERE CustomerID = @PEEP_PK)
 SET @LNAME = (SELECT CUSTOMERLNAME FROM WORKING_PEEPS_Customers WHERE CustomerID = @PEEP_PK)
@@ -91,10 +91,10 @@ SET @PEMAIL = (SELECT EMAIL FROM WORKING_PEEPS_Customers WHERE CustomerID = @PEE
 
 
 EXEC insertPassenger
-@Firsty = @FNAME, 
-@Lasty  = @LNAME, 
-@Birthy = @DOBBY, 
-@Emaily = @PEMAIL, 
+@Firsty = @FNAME,
+@Lasty  = @LNAME,
+@Birthy = @DOBBY,
+@Emaily = @PEMAIL,
 @ptypeName = @PASSTYPNAME
 
 set @RUN = @RUN - 1
@@ -118,12 +118,13 @@ Select * from WORKING_PEEPS_Customers
 
 -- Populate tblTransportation!!!
 -- vehicle, route, employee
-CREATE PROCEDURE proj01_insert_xport
+ALTER PROCEDURE proj01_insert_xport
 @VeName varchar(50),
 @RouName varchar(50),
 @EFName varchar(50),
 @ELName varchar(50),
-@EmpDOB DATE
+@EmpDOB DATE,
+@TName varchar(30)
 AS
 DECLARE @V_ID INT, @E_ID INT, @R_ID INT
 
@@ -160,8 +161,8 @@ IF @E_ID IS NULL
 	END
 
 BEGIN TRAN T1
-INSERT INTO tblTRANSPORTATION(VehicleID, RouteID, EmployeeID)
-	VALUES (@V_ID, @R_ID, @E_ID)
+INSERT INTO tblTRANSPORTATION(VehicleID, RouteID, EmployeeID, transportationName)
+	VALUES (@V_ID, @R_ID, @E_ID, @TName)
 
 IF @@ERROR <> 0
 	BEGIN
@@ -181,7 +182,8 @@ DECLARE @R_PK INT
 DECLARE @R_COUNT INT = (SELECT COUNT(*) FROM tblROUTE)
 DECLARE @V_PK INT
 DECLARE @V_COUNT INT = (Select COUNT(*) from tblVEHICLE)
-DECLARE @VN varchar(50), @RN varchar(30), @EFN varchar(30), @ELN varchar(30), @EDOB Date
+DECLARE @randomDummy varchar(30)
+DECLARE @VN varchar(50), @RN varchar(30), @EFN varchar(30), @ELN varchar(30), @EDOB Date, @TN varchar(30)
 
 
 WHILE @RUN > 0
@@ -198,18 +200,24 @@ BEGIN
     SET @V_PK = (SELECT RAND() * @V_COUNT + 1)
     SET @VN = (SELECT VehicleName from tblVEHICLE WHERE VehicleID = @V_PK)
 
+    SET @randomDummy = (SELECT RAND() * 100000 + 1)
+    SET @TN = convert(varchar, getdate(), 12) + ' ' + @randomDummy
+
 EXEC proj01_insert_xport
 @VeName = @VN,
 @RouName = @RN,
 @EFName = @EFN,
 @ELName = @ELN,
-@EmpDOB = @EDOB
+@EmpDOB = @EDOB,
+@TName = @TN
 
 SET @RUN = @RUN - 1
 
 END
 GO
 
+EXEC wrapper_insertXport 4000
+SELECT * FROM tblTRANSPORTATION
 
 
 	Select * from tblTRANSPORTATION
@@ -219,3 +227,5 @@ GO
 
 	Select * from tblBOARDING
 	Select * from tblTRANSPORTATION
+
+
